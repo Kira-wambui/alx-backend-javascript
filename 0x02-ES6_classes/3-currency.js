2885ae4740f2346844d7e9f1877ed86d3896d4c8
Kira-wambui <nancykira21@gmail.com>
2023-09-28 19:48:13 +0000

    ES6 classes

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,15 @@
+export default class Currency {
+  /**
+   *
+   * @param {String} code
+   * @param {String} name
+   */
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,21 @@
+export default class Building {
+  /**
+   *
+   * @param {Number} sqft
+   */
+  constructor(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('Sqft must be a number');
+    }
+
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
