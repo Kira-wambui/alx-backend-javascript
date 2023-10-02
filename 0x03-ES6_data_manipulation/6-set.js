c6f07e7dfd1ec9a2588893d651b0f5dc1f8133b4
Kira-wambui <nancykira21@gmail.com>
2023-10-02 16:47:27 +0000

    data manipulation

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * Creates a set from an array.
+ * @param {Array} array - The source array.
+ * @author NANCY NJUGUNA <https://github.com/Kira-wambui>
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
