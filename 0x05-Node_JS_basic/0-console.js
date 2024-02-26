5229cb464a3a6fb9c91ce055f90c0cd7f6398536
Kira-wambui <nancykira21@gmail.com>
2024-02-26 20:24:03 +0300

    Node JS basic

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Nancy Wambui <https://github.com/Kira-wambui>
+ */
+const displayMessage = (msg) => {
+    console.log(msg);
+  };
+  
+  module.exports = displayMessage;
